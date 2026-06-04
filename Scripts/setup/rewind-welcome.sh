a922a3202e5716e473f5267e7fc3aca3a516e1cd
Nam <henrynguyen9117@gmail.com>
2026-06-04 10:39:20 +0700

    Add Rewind Welcome prototype

---
+++ Scripts/setup/rewind-welcome.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "================================="
+echo "      Welcome to Rewind OS"
+echo "================================="
+
+echo "1. Setup my computer"
+echo "2. View documentation"
+echo "3. Exit"
+
+read -r OPTION
+
+case $OPTION in
+    1)
+        ./profile-menu.sh
+        ;;
+    2)
+        echo "Opening Kineat Base..."
+        echo "Documentation is under construction"
+        ;;
+    3)
+        exit
+        ;;
+    *)
+        echo "Invalid option"
+        ;;
+esac
